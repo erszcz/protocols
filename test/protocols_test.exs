056f8d0e64330133defd18ce9ab974dd151066cc
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-08-09 14:31:19 +0200

    Test JTD usage

--- test/protocols_test.exs
+++ test/protocols_test.exs
@@ -1,8 +1,38 @@
 defmodule ProtocolsTest do
   use ExUnit.Case
-  doctest Protocols
 
-  test "greets the world" do
-    assert Protocols.hello() == :world
+  describe "JTD" do
+    @user_schema Poison.decode!("""
+                 {
+                   "properties": {
+                     "id": { "type": "string" },
+                     "createdAt": { "type": "timestamp" },
+                     "karma": { "type": "int32" },
+                     "isAdmin": { "type": "boolean" }
+                   }
+                 }
+                 """)
+                 |> JTD.Schema.from_map()
+    @invalid_user Poison.decode!("""
+                  {
+                    "id": 123
+                  }
+                  """)
+    @valid_user Poison.decode!("""
+                {
+                 "id": "1",
+                 "createdAt": "2023-08-09T14:05:22Z",
+                 "karma": 123,
+                 "isAdmin": false
+                }
+                """)
+
+    test "validates a valid object without errors" do
+      assert [] == JTD.validate(@user_schema, @valid_user)
+    end
+
+    test "returns validation errors for an invalid object" do
+      assert match?([_ | _], JTD.validate(@user_schema, @invalid_user))
+    end
   end
 end
